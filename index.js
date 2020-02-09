40ee13ebeda8a55bbcf829656428d20c5e54c51b
Romane G <romane71193@gmail.com>
2020-02-09 11:30:32 -0500

    paths split

--- index.js
+++ index.js
@@ -2,13 +2,18 @@
 const patternMatch = file = () => {
     let args = process.argv.slice([2]);
     //console.log(`${args}`);
-    console.log(args)
+    //console.log(args)
     let numberOfPatterns = args[0];
     let numberOfPaths = args[2]
     let patterns = [args[1]]
     let paths = [args[3]]
-    console.log(numberOfPatterns,numberOfPaths)
-    console.log("Patterns:",patterns,"Paths:",paths);
+    //console.log(numberOfPatterns,numberOfPaths)
+    //console.log("Patterns:",patterns,"Paths:",paths);
+
+    for(let i = 0; i < paths.length; i++){
+        noSlash = paths.toString().split('/').join('')
+        console.log(noSlash);
+    }
 }
 
 patternMatch("right now");
