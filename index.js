f92c94a55ab9dd8420e565c59358635f705621e5
Romane G <romane71193@gmail.com>
2020-02-08 21:24:59 -0500

    lets return an array

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 
 const patternMatch = file = () => {
     const args = process.argv.slice([2]);
-    console.log(`Lets pattern match ${args}`);
+    //console.log(`${args}`);
+    console.log(args)
 }
 
 patternMatch("right now");
