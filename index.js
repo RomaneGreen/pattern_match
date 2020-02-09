0d63b9164f898af4430d2ff9bb2c475153d8fb33
Romane G <romane71193@gmail.com>
2020-02-08 21:42:50 -0500

    patterns n paths

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 
 const patternMatch = file = () => {
-    const args = process.argv.slice([2]);
+    let args = process.argv.slice([2]);
     //console.log(`${args}`);
     console.log(args)
+    let numberOfPatterns = args[0];
+    let numberOfPaths = args[2]
+    let patterns = [args[1]]
+    let paths = [args[3]]
+    console.log(numberOfPatterns,numberOfPaths)
+    console.log("Patterns:",patterns,"Paths:",paths);
 }
 
 patternMatch("right now");
+
+
+//console.log(calls.join("\n"));
