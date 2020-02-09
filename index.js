1cce54ef6ce83bfff7e9be22068b8646e1ff2ed9
Romane G <romane71193@gmail.com>
2020-02-08 20:04:04 -0500

    grabs first command line argument

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Lets pattern match")
+
+const patternMatch = file = () => {
+    const args = process.argv.slice([2]);
+    console.log(`Lets pattern match ${args}`);
+}
+
+patternMatch("right now");
